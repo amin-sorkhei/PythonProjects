7a02dbecf482539b5cca9aa007dc1a76217ba82c
Amin <amin.sorkhei@gmail.com>
2015-04-12 15:19:41 +0300

    rule generation

--- DataMining/main.py
+++ DataMining/main.py
@@ -3,27 +3,11 @@ from course import student_course
 import re
 import itertools
 import time
+import pickle
+
 
 support_count_dic = {}
-
-
-def rule_generator(all_courses, size_of_the_set, students_list):
-    set_count_dic = {}
-    n_subset = itertools.combinations(all_courses, size_of_the_set)
-    for elem in n_subset:
-        support = support_counter(list(elem), students_list)
-        if support > 0:
-            set_count_dic[','.join(list(elem))] = support
-
-    return set_count_dic
-
-
-def support_counter(list_of_courses, student_list):
-    cnt = 0
-    for std in student_list:
-        if set(list_of_courses).issubset(set(std.courses)):
-            cnt += 1
-    return cnt / float(len(student_list))
+closed_frequent_itemsets = []
 
 
 def read():
@@ -57,9 +41,6 @@ def read():
 
 def support_count(list_of_items, transactions, threshold):
     result = []
-    print '--counting support'
-    # print 'This is the size ' + str(len(support_count_dic.items()))
-    # print 'beginning of support count for ' + str(len(list_of_items[0])) + '-itemset'
     for item in list_of_items:
         _item = item
         count = 0
@@ -69,9 +50,9 @@ def support_count(list_of_items, transactions, threshold):
             if set(_item) <= set(transaction):
                     count += 1
         support = float(count) / len(transactions)
-        # print str(item) + ' ' + str(support)
-        if support >= threshold:
+        if frozenset(_item) not in support_count_dic.keys():
             support_count_dic[frozenset(_item)] = support
+        if support >= threshold:
             result.append(item)
     result.sort()
     return result
@@ -87,12 +68,27 @@ def merge(list_of_items):
     return result
 
 
+def closed_set_finder(list_of_parents, list_of_new_children):
+    print 'These are parents ' + str(list_of_parents)
+    print 'These are children ' + str(list_of_new_children)
+    for parent in list_of_parents:
+        children = filter(lambda x: set(parent) <= set(x), list_of_new_children)
+        print str(parent) + ' => ' + str(children)
+        matched_children = filter(lambda child: support_count_dic[frozenset(parent)] == support_count_dic[frozenset(child)], children)
+        if not matched_children and children:
+            print 'closed frequent item set found ' + str(parent)
+            closed_frequent_itemsets.append(parent)
+
+
 def item_gen(items, transactions, threshold):
     initial = support_count(items, transactions, threshold)
     two_candidates = map(list, list(itertools.combinations(initial, 2)))
     print 'length of two-candidates: ' + str(len(two_candidates))
     final_items = two_candidates
+    # since initial contains elements which are not list, we need to turn them to lists
+    initial = map(lambda x: x.split(), initial)
     new_candidates = support_count(two_candidates, transactions, threshold)
+    closed_set_finder(initial, new_candidates)
     while new_candidates:
         final_items = new_candidates
         print 'length of best answers so far: ' + str(len(final_items)) + ' and the length of the itemset is ' + str(len(final_items[0]))
@@ -100,7 +96,48 @@ def item_gen(items, transactions, threshold):
         print 'size of new candidates ' + str(len(new_candidates))
         if new_candidates:
             new_candidates = support_count(new_candidates, transactions, threshold)
+            closed_set_finder(final_items, new_candidates)
     return final_items
+
+
+def rule_generator(all_freqent_itemsets, conf):
+    for frequent_itemset in all_freqent_itemsets:
+        one_level_rule_gen(frequent_itemset, conf)
+
+
+def one_level_rule_gen(itemset, conf):
+    consequent = itemset
+    for item in itemset:
+        head = item.split()
+        precedent = [item for item in itemset if item not in head]
+        rule_conf = support_count_dic[frozenset(head + precedent)]/support_count_dic[frozenset(precedent)]
+        if rule_conf < conf:
+            consequent = list(set(consequent) - set(head))
+        print str(precedent) + ' => ' + str(head) + '  ' + str(rule_conf)
+    consequent = map(list, list(itertools.combinations(consequent, 2)))
+    print 'First set of consequents ' + str(consequent)
+    while consequent:
+        temp_consequent = consequent
+        for head in temp_consequent:
+            precedent = [item for item in itemset if item not in head]
+            if not precedent:
+                break
+            rule_conf = support_count_dic[frozenset(head + precedent)]/support_count_dic[frozenset(precedent)]
+            if rule_conf < conf:
+                consequent = [x for x in consequent if x != head]
+            print str(precedent) + ' => ' + str(head) + '  ' + str(rule_conf)
+        consequent = merge(consequent)
+        print 'This is the list of new consequents ' + str(consequent)
+
+
+def print_dic():
+    f_str = ''
+    keys = map(list, support_count_dic.keys())
+    keys.sort(lambda x, y: cmp(len(x), len(y)))
+    print len(keys)
+    for k in keys:
+        f_str += str(k) + ' ' + str(support_count_dic[frozenset(k)]) + '\n'
+    return f_str
 
 
 def main():
@@ -116,6 +153,11 @@ def main():
     print 'This is the elapsed time ' + str(end - start)
     for itemset in final_items:
         print 'This is the support for ' + str(itemset) + ' ' + str((support_count_dic[frozenset(itemset)]))
+    final_frequent_closed_itemsets = closed_frequent_itemsets + final_items
+
+    pickle.dump(support_count_dic, open('dic.pic', 'w'))
+    open('dic.txt', 'w').write(print_dic())
+    print(final_frequent_closed_itemsets)
 
 
 if __name__ == '__main__':
